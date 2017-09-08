7549dbffc2fd72a930e79ee641b6079cb0b1ccf3
ludek.frey <ludek.frey@gmail.com>
2017-09-08 23:10:51 +0200

    model

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
