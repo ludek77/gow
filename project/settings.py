74ac2f27f424f4371b9e19233cb3f49eb5bbe1ec
ludek.frey <ludek.frey@gmail.com>
2017-09-09 07:20:23 +0200

    undo tst

--- project/settings.py
+++ project/settings.py
@@ -107,7 +107,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
