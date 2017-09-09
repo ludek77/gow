53179907acc0591ac941cdf350fb54375ff60f79
ludek.frey <ludek.frey@gmail.com>
2017-09-09 07:11:13 +0200

    views

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from ui.views import index
-
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^ui/', include('ui.urls')),
 ]
