6cd8d07af34296e5e26510523be32eca26a513d3
ludek.frey <ludek.frey@gmail.com>
2017-09-09 10:33:53 +0200

    auth

--- ui/urls.py
+++ ui/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index),
+    url(r'^logout/', views.logout_view),
+    url(r'^login/', views.login_view),
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, logout, login
 from django.http import HttpResponse
 
+def logout_view(request):
+    logout(request)
+    return HttpResponse("Logged out")
+
+def login_view(request):
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Login "+username)
+    else:
+        return HttpResponse("Invalid login")
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.user.is_authenticated:
+        return HttpResponse("Hello "+request.user.username)
+    else:
+        return HttpResponse("Who are you")
